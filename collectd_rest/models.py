f612bac0815148921261b7a643183f3c2a5fc20d
Matwey V. Kornilov <matwey@sai.msu.ru>
2014-11-27 12:48:41 +0000

    Use rrd.RRDError instead of subprocess exception

--- collectd_rest/models.py
+++ collectd_rest/models.py
@@ -1,7 +1,6 @@
-from collectd_rest.rrd import render
+from collectd_rest.rrd import render, RRDError
 from django.db import models
 from django.core.exceptions import ValidationError
-import subprocess
 
 class GraphGroup(models.Model):
 	name = models.CharField(max_length=256, blank = False, null = True, unique = True)
@@ -20,6 +19,6 @@ class Graph(models.Model):
 	def clean(self):
 		try:
 			render(self.command, 'PNG')
-		except subprocess.CalledProcessError as e:
-			raise ValidationError({'command':e.output})
+		except RRDError as e:
+			raise ValidationError({'command':e.message})
 
--- collectd_rest/rrd.py
+++ collectd_rest/rrd.py
@@ -1,9 +1,13 @@
 import shlex, subprocess
 
+class RRDError(Exception):
+	def __init__(self, message):
+		self.message = message
+
 def render(command, format):
 	args = ['rrdtool','graph','-','--imgformat',format] + shlex.split(command)
 	p = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	code = p.wait()
 	if code != 0:
-		raise subprocess.CalledProcessError(code, p.args, p.stderr.read())
+		raise RRDError(p.stderr.read())
 	return p.stdout.read()
