794880773c77033a8ed7ef4a369b70cf63fc3c54
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2018-01-18 11:59:55 +0300

    Use models.SlugField for name in Graph an GraphGroup

--- collectd_rest/models.py
+++ collectd_rest/models.py
@@ -2,11 +2,11 @@ from collectd_rest.rrd import render
 from django.db import models
 
 class GraphGroup(models.Model):
-	name = models.CharField(max_length=256, blank = False, unique = True)
+	name = models.SlugField(max_length=256, blank = False, unique = True)
 	title = models.CharField(max_length=256, blank = False)
 
 class Graph(models.Model):
-	name = models.CharField(max_length=256, blank = False)
+	name = models.SlugField(max_length=256, blank = False)
 	title = models.CharField(max_length=256, blank = True)
 	priority = models.IntegerField(default=0)
 	group = models.ForeignKey('GraphGroup', on_delete=models.CASCADE, related_name='graphs')
