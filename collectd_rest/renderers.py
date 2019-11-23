fde7d74fe5c942215d150461a5bd15766e93a846
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2019-11-23 22:17:30 +0300

    Check response status at ImageRenderer.render
    Fix #5

--- collectd_rest/renderers.py
+++ collectd_rest/renderers.py
@@ -1,14 +1,20 @@
 from django.utils.cache import patch_cache_control
-from rest_framework import renderers
+from rest_framework import renderers, status
 
 class ImageRenderer(renderers.BaseRenderer):
 	charset = None
 	render_style = 'binary'
 
 	def render(self, data, media_type=None, renderer_context=None):
-		if renderer_context is not None:
-			response = renderer_context['response']
-			patch_cache_control(response, max_age=data['max_age'], must_revalidate=True)
+		if renderer_context is None:
+			return None
+
+		response = renderer_context['response']
+		status_code = renderer_context['response'].status_code
+		if not status.is_success(status_code):
+			return None
+
+		patch_cache_control(response, max_age=data['max_age'], must_revalidate=True)
 		return data['command']
 
 class PNGRenderer(ImageRenderer):
--- tests/graph.py
+++ tests/graph.py
@@ -163,3 +163,13 @@ class GraphTest(TestCase):
 			'command': command}, format='json')
 		self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 		self.assertEqual(response.content,b"{\"command\":[\"Boom!\"]}")
+
+	def test_missing_graph_404_detail(self):
+		url = reverse('graph-detail', args=[404])
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+	def test_missing_graph_404_png(self):
+		url = reverse('graph-detail', args=[404])
+		response = self.client.get(url, HTTP_ACCEPT="image/png")
+		self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
