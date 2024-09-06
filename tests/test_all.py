a6861310ddbe723be77db8d62c5540d190c80a2f
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2024-09-06 23:00:06 +0300

    Initial implementation for pyproject.toml

--- tests/test_all.py
+++
@@ -1,6 +0,0 @@
-from tests.graph import GraphTest
-from tests.granularity import GraphGranularityTest
-from tests.group import GraphGroupTest
-from tests.rrd import RRDTest
-
-__all__ = ['GraphTest', 'GraphGranularityTest', 'GraphGroupTest', 'RRDTest']
